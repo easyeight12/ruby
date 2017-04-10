62931380e994b5d75f094d26a27e270ed9726c70
Vladimir Perevyasko <easy_eight@outlook.com>
2017-04-10 19:21:15 +0300

    Add files via upload

--- temp1.1.rb
+++ temp1.1.rb
@@ -1,8 +1,11 @@
 puts "please put temperature"
-data = gets.chomp.upcase
-deg, measure = data.split(/(?=[CF])/)
+      data = gets.chomp.upcase
+      deg, measure = data.split(/(?=[CF])/)
 case measure
-when 'C' then puts 1.8 * deg.to_f + 32
-when 'F' then puts (deg.to_f - 32) / 1.8
-else puts "wtf is this, you cant put effing temp or what?"
+     when 'C' 
+       then puts 1.8 * deg.to_f + 32
+     when 'F' 
+       then puts (deg.to_f - 32) / 1.8
+else 
+puts "wtf is this, you cant put effing temp or what?"
 end
